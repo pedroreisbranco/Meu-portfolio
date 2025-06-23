e7d73d222142e581cd2140a8ba6fa833bda267a9
pedroreisbranco <167046868+pedroreisbranco@users.noreply.github.com>
2025-06-23 15:09:36 -0300

    Deploy do portfólio

--- app/layouts/navbar/nav-data.js
+++ app/layouts/navbar/nav-data.js
@@ -10,26 +10,12 @@ export const navLinks = [
     pathname: '/#details',
   },
   {
-    label: 'Articles',
-    pathname: '/articles',
-  },
-  {
     label: 'Contact',
     pathname: '/contact',
   },
 ];
 
 export const socialLinks = [
-  {
-    label: 'Bluesky',
-    url: `https://bsky.app/profile/${config.bluesky}`,
-    icon: 'bluesky',
-  },
-  {
-    label: 'Figma',
-    url: `https://www.figma.com/${config.figma}`,
-    icon: 'figma',
-  },
   {
     label: 'Github',
     url: `https://github.com/${config.github}`,
